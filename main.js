de0ea615ebd11a9f2125620b5fb85272e581eda7
Tarek Hasan <t.r.hasan33@gmail.com>
2025-05-03 00:21:28 +0600

    added main.js and in which used typed.js to animate the home page text

---
+++ main.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  new Typed(".typed-text", {
+    strings: ["it's <span class='intro-name'>tarek</span> here..."],
+    typeSpeed: 60,
+    showCursor: false,
+    contentType: "html",
+  });
+});
